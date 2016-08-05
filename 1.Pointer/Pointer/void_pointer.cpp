abecff8c13ebe7f080634e2d5179a26f70dc8efe
sunmi <cutysunmi@gmail.com>
2016-08-05 16:25:02 +0900

    Move source codes from new folder

---
+++ 1.Pointer/Pointer/void_pointer.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	int num = 10;
+	char ctype = 'b';
+	int *numPtr = &num;
+	char *cPtr = &ctype;
+
+	void *ptr;
+
+	ptr = numPtr;
+	printf("%d\n", *ptr);    //에러 : void 포인터는 역참조 않됨 
+	ptr = cPtr;
+	printf("%c\n", *ptr);
+
+	//numPtr = ptr;     // int 포인터에 void 포인터 저장 X
+	//cPtr = ptr;		  // char 포인터에 void 포인터 저장 X
+
+	return 0;
+}
