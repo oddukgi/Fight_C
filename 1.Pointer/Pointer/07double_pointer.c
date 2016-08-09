9ca6c070f2668a8494fd92b0b00c76062e2eb4d9
sunmi <cutysunmi@gmail.com>
2016-08-09 18:09:18 +0900

    change pointer example files

---
+++ 1.Pointer/Pointer/07double_pointer.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+int main()
+{
+	int *numPtr1;
+	int **numPtr2;
+	int num = 10;
+	numPtr1 = &num;				//num의 메모리 주소 저장
+	numPtr2 = &numPtr1;			//numPtr1의 메모리 주소 저장
+
+	printf("%d\n", **numPtr2);   //포인터를 두번 역참조하여 num의 메모리 접근
+
+	/*
+	int **numPtr2;       // pointer to pointer to int 
+	numPtr2 --> numPtr1 메모리 주소 --> num 메모리주소
+	
+	
+	*/
+
+	return 0;
+}
